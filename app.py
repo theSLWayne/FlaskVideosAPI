3322ab6ccb372821492371102bd771ee9b8c7619
theSLWayne <hiranhasanka@gmail.com>
2021-07-04 23:12:25 +0530

    Added base video resource

--- app.py
+++ app.py
@@ -4,5 +4,13 @@ from flask_restful import Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
+videos = {}
+
+class Video(Resource):
+    def get(self, video_id):
+        return videos[video_id]
+
+api.add_resource(Video, '/video/<int:video_id>')
+
 if __name__ == '__main__':
     app.run(debug = True)
