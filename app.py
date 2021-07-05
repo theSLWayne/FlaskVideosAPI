edaaa2ed7ab4c53a27ac6a2b681a4f1feda4ffb6
theSLWayne <hiranhasanka@gmail.com>
2021-07-05 23:26:39 +0530

    Implemented PUT functionality + argument parser

--- app.py
+++ app.py
@@ -1,14 +1,25 @@
 from flask import Flask
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse
 
 app = Flask(__name__)
 api = Api(app)
+
+put_args = reqparse.RequestParser()
+put_args.add_argument('name', type = str, required = True, help = 'Name of the video is required')
+put_args.add_argument('views', type = int, help = 'Number of views')
+put_args.add_argument('uploader', type = str, required = True, help = 'Uploader of the video is required')
+put_args.add_argument('likes', type = int, help = 'Number of likes')
 
 videos = {}
 
 class Video(Resource):
     def get(self, video_id):
         return videos[video_id]
+
+    def put(self, video_id):
+        args = put_args.parse_args()
+        videos[video_id] = args
+        return videos[video_id], 201
 
 api.add_resource(Video, '/video/<int:video_id>')
 
