e49fa8381857c28f0b5c1b393736549d88941c60
theSLWayne <hiranhasanka@gmail.com>
2021-07-06 02:49:33 +0530

    Implemented delete function

--- app.py
+++ app.py
@@ -26,6 +26,11 @@ class Video(Resource):
         videos[video_id] = args
         return videos[video_id], 201
 
+    def delete(self, video_id):
+        abort_if_video_doesnt_exist(video_id)
+        del videos[video_id]
+        return '', 204
+
 api.add_resource(Video, '/video/<int:video_id>')
 
 if __name__ == '__main__':
