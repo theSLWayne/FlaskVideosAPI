fbfa908996e6a3e2d0201a351ad902e2f6cfefbd
theSLWayne <hiranhasanka@gmail.com>
2021-10-17 20:44:30 +0530

    New test cases

--- test_api.py
+++ test_api.py
@@ -2,10 +2,28 @@ import requests
 
 localhost = 'http://127.0.0.1:5000/'
 
-# Create new video and print response
-res = requests.put(localhost + '/video/1', {"name": "In the zoo", "uploader": "zooman"})
+#res = requests.post(localhost + '/users', {"name": "PewDewPie", "views": 12, "uploader": "1", "likes": 0})
+#print(res.json())
+
+# Create new uploader and print response
+#res = requests.post(localhost + '/uploaders', {"uploader_name": "PweDiePie", "uploader_email": "uploader@pewdiepie.sw", "uploader_password": "123456"})
+#print(res.json())
+
+res = requests.get(localhost + '/uploaders')
 print(res.json())
 
-# Get the new video added and print response
-res = requests.get(localhost + '/video/1')
+# Wrong uploader name; edit it
+res = requests.put(localhost + '/uploader/1', {"uploader_name": "PewDiePie"})
 print(res.json())
+
+#res = requests.post(localhost + '/uploaders', {"uploader_name": "Maniya", "uploader_email": "kapilau@maniya.lk", "uploader_password": "piefm"})
+#print(res.json())
+
+# Make sure everything worked
+#res = requests.get(localhost + '/uploader/1')
+#print(res.json())
+
+res = requests.delete(localhost + '/uploader/3')
+#print(res.json())
+
+res = requests.get(localhost + '/uploaders')
